176772b2132b54eafe1a1f7dfa4da819d614b351
mavenkim2 <mavenkim2@gmail.com>
2024-09-27 10:22:22 -0400

    event count non block condition variable

--- src/bvh/parallel.h
+++ src/bvh/parallel.h
@@ -119,6 +119,173 @@ struct JobDeque
     }
 };
 
+// Lock-free condition variable
+// https://gitlab.com/libeigen/eigen/-/blob/master/Eigen/src/ThreadPool/EventCount.h
+struct EventCount
+{
+    static const u32 MAX_THREAD_COUNT = 128;
+    static const u64 waiterBits       = 14;
+    static const u64 stackMask        = (1ull << kWaiterBits) - 1;
+    static const u64 waiterShift      = kWaiterBits;
+    static const u64 waiterMask       = ((1ull << kWaiterBits) - 1) << kWaiterShift;
+    static const u64 waiterInc        = 1ull << kWaiterShift;
+    static const u64 signalShift      = 2 * kWaiterBits;
+    static const u64 signalMask       = ((1ull << kWaiterBits) - 1) << kSignalShift;
+    static const u64 signalInc        = 1ull << kSignalShift;
+    static const u64 epochShift       = 3 * kWaiterBits;
+    static const u64 epochBits        = 64 - kEpochShift;
+    static const u64 epochMask        = ((1ull << kEpochBits) - 1) << kEpochShift;
+    static const u64 epochInc         = 1ull << kEpochShift;
+
+    struct Waiter
+    {
+        enum
+        {
+            NOT_SIGNALED,
+            WAITING,
+            SIGNALED,
+        };
+        std::mutex mutex;
+        std::condition_variable cv;
+        alignas(2 * CACHE_LINE_SIZE) std::atomic<u64> next{stackMask};
+        u64 epoch = 0;
+        u32 state = NOT_SIGNALED;
+    };
+
+    Waiter waiters[MAX_THREAD_COUNT];
+    alignas(CACHE_LINE_SIZE * 2) std::atomic<u64> state{stackMask};
+
+    void Prewait()
+    {
+        u64 s = state.load(std::memory_order_relaxed);
+        for (;;)
+        {
+            u64 newState = s + waiterInc;
+            if (state.compare_exchange_weak(s, newState, std::memory_order_seq_cst)) return;
+        }
+    }
+    void CommitWait(Waiter *w)
+    {
+        w->state     = Waiter::NOT_SIGNALED;
+        const u64 me = (w - waiters) | w->epoch;
+        u64 s        = state.load(std::memory_order_seq_cst);
+        for (;;)
+        {
+            u64 newState;
+            if ((s & signalMask) != 0)
+            {
+                newState = s - waiterInc - signalInc;
+                if (state.compare_exchange_weak(s, newState, std::memory_order_acq_rel)) return;
+            }
+            else
+            {
+                newState = ((s & waiterMask) - waiterInc) | me;
+                w->next.store(s & (stackMask | epochMask), std::memory_order_relaxed);
+                if (state.compare_exchange_weak(s, newState, std::memory_order_acq_rel))
+                {
+                    w->epoch += kEpochInc;
+                    Park(w);
+                    return;
+                }
+            }
+        }
+    }
+    void CancelWait()
+    {
+        u64 s = state.load(std::memory_order_relaxed);
+        for (;;)
+        {
+            u64 newState = s - waiterInc;
+            if (((s & waiterMask) >> waiterShift) == ((s & signalMask) >> signalShift)) newState -= signalInc;
+            if (state.compare_exchange_weak(s, newState, std::memory_order_acq_rel)) return;
+        }
+    }
+    void NotifyAll()
+    {
+        std::atomic_thread_fence(std::memory_order_seq_cst);
+        u64 s = state.load(std::memory_order_acquire);
+        for (;;)
+        {
+            const u64 numWaiters = (state & waiterMask) >> waiterShift;
+            const u64 numSignals = (state & signalMask) >> signalShift;
+            if ((state & stackMask) == stackMask && numWaiters == numSignals) return;
+            // Sets the number of signals = to the number of waiters
+            u64 newState = (state & waiterMask) | (numWaiters << signalShift) | stackMask;
+            if (state.compare_exchange_weak(state, newState, std::memory_order_acq_rel))
+            {
+                u64 stackTop = s & stackMask;
+                if (stackTop == stackMask) return;
+                Waiter *w = &waiters[stackTop];
+
+                Unpark(w);
+                return;
+            }
+        }
+    };
+
+    void NotifyOne()
+    {
+        std::atomic_thread_fence(std::memory_order_seq_cst);
+        u64 s = state.load(std::memory_order_acquire);
+        for (;;)
+        {
+            const u64 numWaiters = (state & waiterMask) >> waiterShift;
+            const u64 numSignals = (state & signalMask) >> signalShift;
+            if ((state & stackMask) == stackMask && numWaiters == numSignals) return;
+            u64 newState;
+            if (numSignals < numWaiters)
+            {
+                newState = s + signalInc;
+                if (state.compare_exchange_weak(state, newState, std::memory_order_acq_rel)) return;
+            }
+            else
+            {
+                Waiter *w = &waiters[s & stackMask];
+                u64 next  = w->next.load(std::memory_order_relaxed);
+                newState  = (s & (waiterMask | signalMask)) | next;
+                if (state.compare_exchange_weak(state, newState, std::memory_order_acq_rel))
+                {
+                    u64 stackTop = s & stackMask;
+                    if (stackTop == stackMask) return;
+                    Waiter *w = &waiters[stackTop];
+                    // Next = stackMask means there's no next
+                    w->next.store(stackMask, std::memory_order_relaxed);
+                    Unpark(w);
+                    return;
+                }
+            }
+        }
+    }
+
+    void Park(Waiter *w)
+    {
+        std::unique_lock<std::mutex> lock(w->mutex);
+        while (w->state != Waiter::SIGNALED)
+        {
+            w->state = Waiter::WAITING;
+            w->cv.wait(lock);
+        }
+    }
+
+    void Unpark(Waiter *w)
+    {
+        for (Waiter *next; w; w = next)
+        {
+            u64 wNext = w->next.load(std::memory_order_relaxed) & stackMask;
+            next      = wNext == stackMask ? 0 : &waiters[wNext];
+
+            u32 state;
+            {
+                std::unique_lock<std::mutex> lock(w->mutex);
+                state    = w->state;
+                w->state = Waiter::SIGNALED;
+            }
+
+            if (state == Waiter::WAITING) w->cv.notify_one();
+        }
+    }
+};
+
 struct Worker
 {
     JobDeque queue;
