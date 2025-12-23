62e2b0e205b0f91c4ff9bc9017af857dde82d281
Maven Kim <mavenkim2@gmail.com>
2025-12-23 15:32:55 -0500

    Coalesce samples in one thread group per VMM for soft assignment

--- src/shaders/path_guiding/wem.hlsl
+++ src/shaders/path_guiding/wem.hlsl
@@ -5,7 +5,7 @@ StructuredBuffer<float3> sampleDirections : register(t1);
 StructuredBuffer<uint> sampleVMMIndices : register(t2);
 
 StructuredBuffer<uint> vmmOffsets : register(t3);
-RWStructuredBuffer<uint> vmmCounts : register(u4);
+StructuredBuffer<uint> vmmCounts : register(t4);
 RWStructuredBuffer<Statistics> vmmStatistics : register(u5);
 
 struct Num 
@@ -15,40 +15,72 @@ struct Num
 
 [[vk::push_constant]] Num num;
 
+groupshared Statistics statistics_;
+
 [numthreads(PATH_GUIDING_GROUP_SIZE, 1, 1)]
 void main(uint3 groupID : SV_GroupID, uint3 dtID : SV_DispatchThreadID, uint groupIndex : SV_GroupIndex)
 {
     uint totalNumSamples = num.num;
-    if (dtID.x >= totalNumSamples) return;
-
-    float3 sampleDirection = sampleDirections[dtID.x];
-    uint vmmIndex = sampleVMMIndices[dtID.x];
+    uint vmmIndex = groupID.x;
+    uint sampleCount = vmmCounts[vmmIndex];
+    uint offset = vmmOffsets[vmmIndex];
     VMM vmm = vmms[vmmIndex];
 
+    if (groupIndex == 0)
+    {
+        for (uint i = 0; i < vmm.numComponents; i++)
+        {
+            statistics_.sumWeights[i] = 0.f;
+        }
+        statistics_.weightedLogLikelihood = 0.f;
+    }
+
+    uint laneIndex = WaveGetLaneIndex();
     Statistics statistics;
-    float V = 0.f;
 
-    for (uint i = 0; i < vmm.numComponents; i++)
+    for (uint sampleIndex = groupIndex; sampleIndex < sampleCount; sampleIndex += PATH_GUIDING_GROUP_SIZE)
     {
-        float cosTheta = dot(sampleDirection, vmm.directions[i]);
-        float norm = CalculateVMFNormalization(vmm.kappas[i]);
-        float v = norm * exp(vmm.kappas[i] * min(cosTheta - 1.f, 0.f));
-        statistics.sumWeights[i] = vmm.weights[i] * v;
-        V += statistics.sumWeights[i];
+        float3 sampleDirection = sampleDirections[sampleIndex];
+        float V = 0.f;
+
+        for (uint componentIndex = 0; componentIndex < vmm.numComponents; componentIndex++)
+        {
+            float cosTheta = dot(sampleDirection, vmm.directions[componentIndex]);
+            float norm = CalculateVMFNormalization(vmm.kappas[componentIndex]);
+            float v = norm * exp(vmm.kappas[componentIndex] * min(cosTheta - 1.f, 0.f));
+            statistics.sumWeights[componentIndex] = vmm.weights[componentIndex] * v;
+
+            V += statistics.sumWeights[componentIndex];
+        }
+
+        // TODO: what do I do here?
+        if (V <= 1e-16f) continue;
+
+        float invV = 1.f / V;
+        for (uint i = 0; i < vmm.numComponents; i++)
+        {
+            statistics.sumWeights[i] *= invV;
+        }
+        statistics.weightedLogLikelihood = log(V); // * sampleWeight
+
+        for (uint componentIndex = 0; componentIndex < vmm.numComponents; componentIndex++)
+        {
+            float softAssignmentWeight = statistics.sumWeights[componentIndex];
+            float sumWeights = WaveActiveSum(softAssignmentWeight);
+            float weightedLogLikelihood = WaveActiveSum(statistics.weightedLogLikelihood);
+
+            if (groupIndex == 0)
+            {
+                statistics_.sumWeights[componentIndex] += sumWeights;
+                statistics_.weightedLogLikelihood += weightedLogLikelihood;
+            }
+        }
     }
 
-    // TODO: what do I do here?
-    if (V <= 1e-16f)
+    if (groupIndex == 0)
     {
-        return;
+        vmmStatistics[vmmIndex] = statistics_;
     }
-
-    float invV = 1.f / V;
-    for (uint i = 0; i < vmm.numComponents; i++)
-    {
-        statistics.sumWeights[i] *= invV;
-    }
-    statistics.weightedLogLikelihood = log(V);
 
 #if 0
     float weightedLogLikelihood = sample.weight * log(V);
@@ -60,11 +92,10 @@ void main(uint3 groupID : SV_GroupID, uint3 dtID : SV_DispatchThreadID, uint gro
         directions[i] = assignments[i] * sample.weight * sampleDirection;
         assignments[i] = assignments[i] * sample.weight;
     }
-#endif
-
     uint index;
     InterlockedAdd(vmmCounts[vmmIndex], 1, index);
     index = vmmOffsets[vmmIndex] + index;
 
     vmmStatistics[index] = statistics;
+#endif
 }
